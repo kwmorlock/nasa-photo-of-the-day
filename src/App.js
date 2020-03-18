5e6aea91897c7aca246bbfb81582a21bede38537
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-18 16:27:42 -0400

    useeffect added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  //state hook
+  const [data, setData] = useState();
+  //use effect
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log('wow', error)
+    })
+  }, [])
+
+
   return (
     <div className="App">
       <p>
